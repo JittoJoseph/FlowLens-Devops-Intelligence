d3acaca795991a8b4dff58ff4c64166b26578d19
Jitto Joseph <jitto.joseph67@gmail.com>
2025-09-03 14:32:34 +0530

    feat: add Flutter release workflow and update application namespace to FlowLens

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_app"
+    namespace = "com.devopsmalayalam.flowlens"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,14 +20,18 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_app"
+        // FlowLens - AI-Powered DevOps Dashboard
+        applicationId = "com.devopsmalayalam.flowlens"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        // App metadata
+        resValue("string", "app_name", "FlowLens")
+        manifestPlaceholders["appName"] = "FlowLens"
     }
 
     buildTypes {
